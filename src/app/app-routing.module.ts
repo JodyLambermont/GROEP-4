663db6a2c52f084f45064015dc4304fa08f0e6ae
Jody Lambermont <jody.lambermont@student.ehb.be>
2019-03-19 20:42:18 +0100

    Pages created
    user login register profile page generated + services generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
   { path: 'admin', loadChildren: './admin/admin.module#AdminPageModule' },
-  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' }
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+  { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' }
 
 ];
 @NgModule({
