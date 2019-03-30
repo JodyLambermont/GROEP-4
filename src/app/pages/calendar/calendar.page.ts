261a9eb42e7967131889012b937efb57046d721a
arnemergan <mergan.arne@hotmail.com>
2019-03-30 14:18:35 +0100

    calendar

--- src/app/pages/calendar/calendar.page.ts
+++ src/app/pages/calendar/calendar.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject,LOCALE_ID } from '@angular/core';
+import{ CalendarComponent } from 'ionic2-calendar/calendar';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-calendar',
@@ -6,13 +11,107 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.page.scss'],
 })
 export class CalendarPage implements OnInit {
+  event = {
+    title:'',
+    desc:'',
+    startTime:'',
+    endTime:'',
+    allDay:false
+  };
+
+  minDate = new Date().toISOString();
+
+  eventSource = [];
   calendar = {
-    mode:'week',
+    mode:'month',
     currentDate:new Date()
   }
-  constructor() { }
+
+  viewTitle = '';
+
+@ViewChild(CalendarComponent) myCal:CalendarComponent;
+
+  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
+
 
   ngOnInit() {
+    this.resetEvent();
+  }
+
+  resetEvent(){
+    this.event = {
+      title:'',
+      desc:'',
+      startTime:new Date().toISOString(),
+      endTime:new Date().toISOString(),
+      allDay:false
+    }
+  }
+
+  addEvent(){
+    let eventCopy = {
+      title:this.event.title,
+      startTime:new Date(this.event.startTime),
+      endTime:new Date(this.event.endTime),
+      desc:this.event.desc,
+      allDay:this.event.allDay
+    }
+    if(eventCopy.allDay){
+      let start = eventCopy.startTime;
+      let end = eventCopy.endTime;
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
+      eventCopy.endTime = new Date(Date.UTC(end.getUTCFullYear(), end.getUTCMonth(), end.getUTCDate() + 1));
+    }
+ 
+    this.eventSource.push(eventCopy);
+    this.myCal.loadEvents();
+    this.resetEvent();
+  }
+
+  changeMode(mode){
+      this.calendar.mode = mode;
+  }
+
+  back(){
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.SlidePrev();
+  }
+
+  next(){
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.SlideNext();
+  }
+
+  today(){
+    this.calendar.currentDate = new Date();
+  }
+
+  async onEventSelected(event){
+      let start = formatDate(event.startTime, 'medium', this.locale);
+      let end = formatDate(event.endTime, 'medium', this.locale);
+    
+      const alert = await this.alertCtrl.create({
+        header: event.title,
+        subHeader: event.desc,
+        message: 'From: ' + start + '<br><br>To: ' + end,
+        buttons: ['OK']
+      });
+      alert.present();
+  }
+  onViewTitleChanged(title){
+    this.viewTitle = title;
+  }
+  onTimeSelected(ev){
+    let selected = new Date(ev.selectedTime);
+    this.event.startTime = selected.toISOString();
+    selected.setHours(selected.getHours() + 1);
+    this.event.endTime = (selected.toISOString());
+  }
+  reloadSource(){
+
+  }
+  onCurrentDateChanged(){
+
   }
 
 }
