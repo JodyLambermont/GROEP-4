f361999c0931a2da59726a6c4b9e7b65a7fe2bb5
Jody Lambermont <jody.lambermont@student.ehb.be>
2019-04-15 16:47:55 +0200

    deleted task entry
    task entry component removed as task entry will be used in tab1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,30 +29,29 @@ const routes: Routes = [
     loadChildren: "./pages/signup/signup.module#SignupPageModule"
   },
   {
-    path: "task-entry",
-    loadChildren: "./pages/task-entry/task-entry.module#TaskEntryPageModule"
-  },
-  {
     path: "calendar",
     loadChildren: "./pages/calendar/calendar.module#CalendarPageModule"
   },
   {
     path: "change-name",
-    loadChildren: "./settings/change-name/change-name.module#ChangeNamePageModule"
+    loadChildren:
+      "./settings/change-name/change-name.module#ChangeNamePageModule"
   },
-  { 
-    path: 'change-password', 
-    loadChildren: './settings/change-password/change-password.module#ChangePasswordPageModule' 
+  {
+    path: "change-password",
+    loadChildren:
+      "./settings/change-password/change-password.module#ChangePasswordPageModule"
   },
-  { 
-    path: 'change-workweek', 
-    loadChildren: './settings/change-workweek/change-workweek.module#ChangeWorkweekPageModule' 
+  {
+    path: "change-workweek",
+    loadChildren:
+      "./settings/change-workweek/change-workweek.module#ChangeWorkweekPageModule"
   },
-  { 
-    path: 'change-notifications', 
-    loadChildren: './settings/change-notifications/change-notifications.module#ChangeNotificationsPageModule' 
+  {
+    path: "change-notifications",
+    loadChildren:
+      "./settings/change-notifications/change-notifications.module#ChangeNotificationsPageModule"
   }
-
 ];
 @NgModule({
   imports: [
